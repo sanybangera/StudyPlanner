93b48fecfd82f66b8a7dd055b2415e9c65a8d526
sanybangera <139966130+sanybangera@users.noreply.github.com>
2026-04-30 21:17:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
-return MaterialApp(
-  title: 'Study Planner',
-  home: LoginScreen(),
-  debugShowCheckedModeBanner: false,
-);
+// lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(const StudyPlannerApp());
+}
+
+class StudyPlannerApp extends StatelessWidget {
+  const StudyPlannerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Study Planner',
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
